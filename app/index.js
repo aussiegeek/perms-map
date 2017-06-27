97e4d5f1a1e3b7b88ddea9f0835b2fd243e64046
Alan Harper <alan@aussiegeek.net>
2017-06-28 08:40:18 +1000

    Switch to StandardJS

--- app/index.js
+++ app/index.js
@@ -1,44 +1,44 @@
-import axios from 'axios';
-import bows from 'bows';
-const log = bows('index');
-import ColorHash from 'color-hash';
-const colorHash = new ColorHash();
-import * as topojson from 'topojson-client';
+import axios from 'axios'
+import bows from 'bows'
+import ColorHash from 'color-hash'
+import * as topojson from 'topojson-client'
+const log = bows('index')
+const colorHash = new ColorHash()
 
-window.L.mapbox.accessToken = 'pk.eyJ1IjoidGhldHVyaW5nbWFjaGluZSIsImEiOiIxMjljNzFlOWNiMjZkZWIyNjYxMjViN2Q5NzRhYWRjYSJ9.-LqvR7mvElcVL3rSkB11mQ';
+window.L.mapbox.accessToken = 'pk.eyJ1IjoidGhldHVyaW5nbWFjaGluZSIsImEiOiIxMjljNzFlOWNiMjZkZWIyNjYxMjViN2Q5NzRhYWRjYSJ9.-LqvR7mvElcVL3rSkB11mQ'
 
 const map = window.L.mapbox.map('map', 'mapbox.streets')
-    .setView([-30, 137], 5);
+    .setView([-30, 137], 5)
 
 axios.get('data/perms.json').then((response) => {
-  const layerGroups = {};
-  response.data.forEach((route)=>{
-    let distance = route.distance;
+  const layerGroups = {}
+  response.data.forEach((route) => {
+    let distance = route.distance
     if (route.distance === '') {
-      distance = 'Unknown';
+      distance = 'Unknown'
     }
 
-    if(layerGroups[distance] === undefined) {
-      layerGroups[distance] = window.L.layerGroup().addTo(map);
+    if (layerGroups[distance] === undefined) {
+      layerGroups[distance] = window.L.layerGroup().addTo(map)
     }
 
-    const layerGroup = layerGroups[distance];
+    const layerGroup = layerGroups[distance]
 
     route.topoJson.forEach(topo => {
       axios.get(topo).then((topoJsonResponse) => {
-        const topoJson = topoJsonResponse.data;
-        const geoJson = topojson.feature(topoJson, topoJson.objects[Object.keys(topoJson.objects)[0]]);
+        const topoJson = topoJsonResponse.data
+        const geoJson = topojson.feature(topoJson, topoJson.objects[Object.keys(topoJson.objects)[0]])
         console.log(route)
         const layer = window.L.geoJson(
           geoJson,
           {style: {color: colorHash.hex(route.name)}}
-        ).bindPopup(`<a href="${route.links[0]}">${route.name}</a><br/>${route.description}`);
-        layerGroup.addLayer(layer);
-      });
-    });
-  });
+        ).bindPopup(`<a href="${route.links[0]}">${route.name}</a><br/>${route.description}`)
+        layerGroup.addLayer(layer)
+      })
+    })
+  })
 
-  window.L.control.layers(null, layerGroups, {collapsed: false}).addTo(map);
+  window.L.control.layers(null, layerGroups, {collapsed: false}).addTo(map)
 }).catch((error) => {
-  log(error);
-});
+  log(error)
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   module: {
     loaders: [
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel'
-    }
-    ],
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
   },
   entry: './app',
   devtool: 'source-map',
